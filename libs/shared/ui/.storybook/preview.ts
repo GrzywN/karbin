392c4e515c2ba4d3cf65c08a857237ba120cb565
Karol Binkowski <starystachu9@gmail.com>
2022-12-17 13:38:29 +0100

    feat(shared-ui): add default dark background to shared-ui Storybook

--- libs/shared/ui/.storybook/preview.ts
+++ libs/shared/ui/.storybook/preview.ts
@@ -1,1 +1,17 @@
 import './styles.css';
+
+export const parameters = {
+  backgrounds: {
+    default: 'dark',
+    values: [
+      {
+        name: 'dark',
+        value: '#262626',
+      },
+      {
+        name: 'light',
+        value: '#ffffff',
+      },
+    ],
+  },
+};
