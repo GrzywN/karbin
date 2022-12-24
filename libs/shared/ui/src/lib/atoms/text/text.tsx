d42f2c2315603fee5b09bd487a3f485d115495b2
Karol Binkowski <starystachu9@gmail.com>
2022-12-24 17:55:45 +0100

    fix(shared-ui): make breakpoints and default classes consistent

--- libs/shared/ui/src/lib/atoms/text/text.tsx
+++ libs/shared/ui/src/lib/atoms/text/text.tsx
@@ -1,14 +1,14 @@
 import { cva, VariantProps } from 'class-variance-authority';
 import type { OverridableComponentProps } from '../../OverridableComponentProps';
 
-const textStyles = cva(['font-sans text-neutral-400', 'font-light'], {
+const textStyles = cva('font-sans font-light', {
   variants: {
     size: {
       xs: 'text-xs sm:text-base lg:text-lg',
       sm: 'text-sm sm:text-lg lg:text-xl',
       md: 'text-base sm:text-xl lg:text-2xl',
-      lg: 'text-lg sm:text-2xl xl:text-3xl',
-      xl: 'text-xl sm:text-3xl xl:text-4xl',
+      lg: 'text-lg sm:text-2xl lg:text-3xl',
+      xl: 'text-xl sm:text-3xl lg:text-4xl',
     },
     link: {
       true: 'cursor-pointer transition-colors hover:text-white focus-visible:text-white',
