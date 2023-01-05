88566e970b108299cc00df21f5745d4f9de49221
Karol Binkowski <starystachu9@gmail.com>
2023-01-05 08:08:50 +0100

    feat(site): add basic site structure (Subject to change)

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Navbar, Logo, NavLinkWithNextRouting } from '@karbin/shared/ui';
 import { Pacifico, Plus_Jakarta_Sans } from '@next/font/google';
 
 import './styles.css';
@@ -24,6 +25,15 @@ function Website({ Component, pageProps }: AppProps) {
       <div
         className={`${pacifico.variable} ${plusJakartaSans.variable} font-sans`}
       >
+        <Navbar>
+          <Logo />
+          <Navbar.Nav>
+            <NavLinkWithNextRouting href="/work" name="Work" />
+            <NavLinkWithNextRouting href="/about" name="About me" />
+            <NavLinkWithNextRouting href="/resume" name="Resume" />
+            <NavLinkWithNextRouting href="/contact" name="Contact" />
+          </Navbar.Nav>
+        </Navbar>
         <main className="max-w-7xl w-full mx-auto py-28 px-8 grid gap-16">
           <Component {...pageProps} />
         </main>
---
+++ apps/site/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-neutral-800 min-h-[101vh]">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
