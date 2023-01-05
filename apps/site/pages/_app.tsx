0698361fd3f4393e1e63a10069e0c156bafb3e6a
Karol Binkowski <starystachu9@gmail.com>
2023-01-05 08:06:08 +0100

    feat(site): setup @next/font, change Inter font to Plus Jakarta Sans

--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -1,18 +1,35 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Pacifico, Plus_Jakarta_Sans } from '@next/font/google';
+
 import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+const pacifico = Pacifico({
+  weight: '400',
+  subsets: ['latin', 'latin-ext'],
+  variable: '--font-pacifico',
+});
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ['latin', 'latin-ext'],
+  variable: '--font-plus-jakarta-sans',
+});
+
+function Website({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Karol Binkowski | Front-end developer</title>
       </Head>
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <div
+        className={`${pacifico.variable} ${plusJakartaSans.variable} font-sans`}
+      >
+        <main className="max-w-7xl w-full mx-auto py-28 px-8 grid gap-16">
+          <Component {...pageProps} />
+        </main>
+      </div>
     </>
   );
 }
 
-export default CustomApp;
+export default Website;
