b8be54462b98d1d5f1e110773bdd1d9562376f04
Karol Binkowski <starystachu9@gmail.com>
2023-01-08 12:53:20 +0100

    style(shared-ui): format Paper e2e test

--- apps/shared-ui-e2e/src/e2e/paper/paper.cy.ts
+++ apps/shared-ui-e2e/src/e2e/paper/paper.cy.ts
@@ -1,6 +1,8 @@
 describe('shared-ui: Paper component', () => {
   beforeEach(() =>
-    cy.visit('/iframe.html?id=design-system-molecules-paper--default&args=classes;children;')
+    cy.visit(
+      '/iframe.html?id=design-system-molecules-paper--default&args=classes;children;'
+    )
   );
 
   it('should render the component', () => {
