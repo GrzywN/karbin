5634736dc4c5d935484bb2e7b49d5e4adbf4b272
Karol Binkowski <starystachu9@gmail.com>
2023-01-15 19:49:01 +0100

    feat(shared-ui): add EmailAndResumeBox component

---
+++ libs/shared/ui/src/lib/molecules/email-and-resume-box/email-and-resume-box.tsx
@@ -0,0 +1,34 @@
+import Paper from '../../molecules/paper/paper';
+import Button from '../../atoms/button/button';
+import Text from '../../atoms/text/text';
+import { IconMail, IconFile } from '@tabler/icons';
+
+export interface EmailAndResumeBoxProps {
+  email: string;
+  resumeFileName: string;
+}
+
+export function EmailAndResumeBox(props: EmailAndResumeBoxProps) {
+  const {
+    email = 'example@example.com',
+    resumeFileName = 'Resume-Jane-Doe.pdf',
+  } = props;
+
+  return (
+    <Paper classes="p-4 gap-4 max-w-md">
+      <Button as="a" href={`mailto:${email}`} color="primary">
+        <IconMail />
+        <Text color="dark" size="xs">
+          {email}
+        </Text>
+      </Button>
+      <Text as="a" href={`/${resumeFileName}`} download color="dark" link>
+        <span className="inline-flex items-center gap-2 underline">
+          <IconFile color="black" /> {resumeFileName}
+        </span>
+      </Text>
+    </Paper>
+  );
+}
+
+export default EmailAndResumeBox;
