eb434b543acf64aaf5e81cb211687f428d0f4d77
Karol Binkowski <starystachu9@gmail.com>
2023-01-16 09:54:19 +0100

    feat(site): add ContactSection component

---
+++ apps/site/components/contact-section/contact-section.tsx
@@ -0,0 +1,23 @@
+import { Section, Heading, EmailAndResumeBox } from '@karbin/shared/ui';
+
+export interface ContactSectionProps {
+  sectionTitle: string;
+  heading: string | React.ReactNode;
+  email: string;
+  resumeFileName: string;
+}
+
+export function ContactSection(props: ContactSectionProps) {
+  const { sectionTitle, heading, email, resumeFileName } = props;
+
+  return (
+    <Section title={sectionTitle}>
+      <Heading as="h2" size="xl">
+        {heading}
+      </Heading>
+      <EmailAndResumeBox email={email} resumeFileName={resumeFileName} />
+    </Section>
+  );
+}
+
+export default ContactSection;
