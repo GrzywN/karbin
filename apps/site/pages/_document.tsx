8d40705e73d800b91863e1d146fc42cf31b06757
Karol Binkowski <starystachu9@gmail.com>
2023-01-16 10:00:32 +0100

    chore(site): remove min-h-[101vh]
    It was necessary for testing navbar hiding on scroll. It's no longer needed.

--- apps/site/pages/_document.tsx
+++ apps/site/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-neutral-800 min-h-[101vh]">
+      <body className="bg-neutral-800">
         <Main />
         <NextScript />
       </body>
