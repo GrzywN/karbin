d9e1a6e730100e7e6863838586a842e1dd04d466
Karol Binkowski <starystachu9@gmail.com>
2023-01-16 09:35:06 +0100

    feat(site): add QuickIntroductionSection component

---
+++ apps/site/components/quick-introduction-section/quick-introduction-section.tsx
@@ -0,0 +1,22 @@
+import { Section, Paper, Text } from '@karbin/shared/ui';
+
+export interface QuickIntroductionSectionProps {
+  sectionTitle: string;
+  text: string | React.ReactNode;
+}
+
+export function QuickIntroductionSection(props: QuickIntroductionSectionProps) {
+  const { sectionTitle, text } = props;
+
+  return (
+    <Section title={sectionTitle}>
+      <Paper classes="grid max-w-xl" color="primary" hasPadding>
+        <Text as="p" size="sm" color="dark" weight="medium">
+          {text}
+        </Text>
+      </Paper>
+    </Section>
+  );
+}
+
+export default QuickIntroductionSection;
