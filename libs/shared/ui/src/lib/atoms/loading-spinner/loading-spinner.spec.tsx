bf1c4e5dded09e81d65fb1ddc769013506d68e4d
Karol Binkowski <starystachu9@gmail.com>
2023-01-28 20:05:03 +0100

    test(shared-ui): add LoadingSpinner specs

--- libs/shared/ui/src/lib/atoms/loading-spinner/loading-spinner.spec.tsx
+++ libs/shared/ui/src/lib/atoms/loading-spinner/loading-spinner.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import LoadingSpinner from './loading-spinner';
 
@@ -6,5 +7,21 @@ describe('LoadingSpinner', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<LoadingSpinner />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should have text content', () => {
+    const text = 'Text';
+    const { baseElement } = render(<LoadingSpinner>{text}</LoadingSpinner>);
+    expect(baseElement).toHaveTextContent(text);
+  });
+
+  it('should have not visible text content', () => {
+    const text = 'Text';
+    const { baseElement } = render(<LoadingSpinner>{text}</LoadingSpinner>);
+    const hiddenElement = screen.queryByText(text);
+
+    expect(baseElement).toHaveTextContent(text);
+    expect(hiddenElement).toBeInTheDocument();
+    expect(hiddenElement).toHaveClass('invisible');
   });
 });
