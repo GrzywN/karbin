de63ab9ae6160e0cb32e4459c12d0b87d757fdaa
Karol Binkowski <starystachu9@gmail.com>
2023-01-28 12:18:25 +0100

    test(site): fix MyToolsSection spec (not passing required props)

--- apps/site/components/my-tools-section/my-tools-section.spec.tsx
+++ apps/site/components/my-tools-section/my-tools-section.spec.tsx
@@ -4,7 +4,13 @@ import MyToolsSection from './my-tools-section';
 
 describe('MyToolsSection', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<MyToolsSection />);
+    const { baseElement } = render(
+      <MyToolsSection
+        sectionTitle="My tools"
+        heading="My tools"
+        categories={[{ heading: 'Lorem', list: ['Lorem', 'Ipsum'] }]}
+      />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
