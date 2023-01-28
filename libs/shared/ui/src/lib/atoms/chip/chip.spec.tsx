e99d75c01f20ba55bb7f9aa1f501ffce05e78959
Karol Binkowski <starystachu9@gmail.com>
2023-01-28 15:08:07 +0100

    test(shared-ui): add Chip specs
    Specs include:
    - Rendering Chip with text passed as a child
    - Rendering chip with ReactNode passed as a child

--- libs/shared/ui/src/lib/atoms/chip/chip.spec.tsx
+++ libs/shared/ui/src/lib/atoms/chip/chip.spec.tsx
@@ -1,10 +1,27 @@
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Chip from './chip';
 
 describe('Chip', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Chip />);
+    const { baseElement } = render(<Chip>Lorem ipsum</Chip>);
+
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render successfully with a text passed as a children', () => {
+    const CHIP_TEXT = 'Chip text';
+    const { baseElement } = render(<Chip>{CHIP_TEXT}</Chip>);
+
+    expect(baseElement).toHaveTextContent(CHIP_TEXT);
+  });
+
+  it('should render successfully with a ReactNode passed as a children', () => {
+    const CHIP_TEXT = 'Chip text maybe with an icon.';
+    const CHIP_NODE = <span>{CHIP_TEXT}</span>;
+    const { baseElement } = render(<Chip>{CHIP_NODE}</Chip>);
+
+    expect(baseElement).toHaveTextContent(CHIP_TEXT);
   });
 });
