035101faf4351e5f3437bd8331d31feca912812b
Karol Binkowski <starystachu9@gmail.com>
2023-01-28 15:20:19 +0100

    test(shared-ui): add Heading specs
    Specs include:
    - Rendering Heading with text passed as a child
    - Rendering Heading with ReactNode passed as a child

--- libs/shared/ui/src/lib/atoms/heading/heading.spec.tsx
+++ libs/shared/ui/src/lib/atoms/heading/heading.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Heading from './heading';
 
@@ -6,5 +7,20 @@ describe('Heading', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Heading />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render successfully with a text passed as a child', () => {
+    const HEADING_TEXT = 'Heading text';
+    const { baseElement } = render(<Heading>{HEADING_TEXT}</Heading>);
+
+    expect(baseElement).toHaveTextContent(HEADING_TEXT);
+  });
+
+  it('should render successfully with a ReactNode passed as a child', () => {
+    const HEADING_TEXT = 'Heading text';
+    const HEADING_NODE = <span>{HEADING_TEXT}</span>;
+    const { baseElement } = render(<Heading>{HEADING_NODE}</Heading>);
+
+    expect(baseElement).toHaveTextContent(HEADING_TEXT);
   });
 });
