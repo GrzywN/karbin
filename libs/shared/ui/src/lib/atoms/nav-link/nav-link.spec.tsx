5fbc40ff0a7219f57e50d3f7f8a5d7882de2de36
Karol Binkowski <starystachu9@gmail.com>
2023-01-29 06:44:12 +0100

    test(shared-ui): add NavLink specs

--- libs/shared/ui/src/lib/atoms/nav-link/nav-link.spec.tsx
+++ libs/shared/ui/src/lib/atoms/nav-link/nav-link.spec.tsx
@@ -1,10 +1,20 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import NavLink from './nav-link';
 
 describe('NavLink', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<NavLink />);
+    const { baseElement } = render(<NavLink name="Link" href="#" />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should trigger an onClick event when clicked', () => {
+    const text = 'Text';
+    const handleClick = jest.fn();
+    render(<NavLink name={text} href="#" onClick={handleClick} />);
+
+    fireEvent.click(screen.getByText(text));
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
