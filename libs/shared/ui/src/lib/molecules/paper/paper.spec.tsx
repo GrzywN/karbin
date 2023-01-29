d69bd40e46c594232013ff2d52fa3b51f0b06e36
Karol Binkowski <starystachu9@gmail.com>
2023-01-29 06:56:08 +0100

    test(shared-ui): add Paper specs

--- libs/shared/ui/src/lib/molecules/paper/paper.spec.tsx
+++ libs/shared/ui/src/lib/molecules/paper/paper.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Paper from './paper';
 
@@ -6,5 +7,20 @@ describe('Paper', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Paper />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render successfully with a text passed as a child', () => {
+    const text = 'Text';
+    const { baseElement } = render(<Paper>{text}</Paper>);
+
+    expect(baseElement).toHaveTextContent(text);
+  });
+
+  it('should render successfully with a ReactNode passed as a child', () => {
+    const text = 'Text';
+    const node = <span>{text}</span>;
+    const { baseElement } = render(<Paper>{node}</Paper>);
+
+    expect(baseElement).toHaveTextContent(text);
   });
 });
