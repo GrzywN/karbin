3d037d34ce246e4f159d4cfa1e95b4ff2dc7cee4
Karol Binkowski <starystachu9@gmail.com>
2023-01-29 06:48:05 +0100

    test(shared-ui): add Text specs

--- libs/shared/ui/src/lib/atoms/text/text.spec.tsx
+++ libs/shared/ui/src/lib/atoms/text/text.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Text from './text';
 
@@ -6,5 +7,30 @@ describe('Text', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Text />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render successfully with a text passed as a child', () => {
+    const text = 'Text';
+    const { baseElement } = render(<Text>{text}</Text>);
+
+    expect(baseElement).toHaveTextContent(text);
+  });
+
+  it('should render successfully with a ReactNode passed as a child', () => {
+    const text = 'Text';
+    const node = <span>{text}</span>;
+    const { baseElement } = render(<Text>{node}</Text>);
+
+    expect(baseElement).toHaveTextContent(text);
+  });
+
+  it('should render as an element passed in "as" prop', () => {
+    const tagName = 'span';
+    const text = 'Text';
+    render(<Text as={tagName}>{text}</Text>);
+
+    expect(
+      screen.getByText(text).tagName === tagName.toUpperCase()
+    ).toBeTruthy();
   });
 });
