a7947db9adbaeb0a009fa12bdc76e5feca6f1a8f
Karol Binkowski <starystachu9@gmail.com>
2023-01-29 07:41:13 +0100

    test(shared-ui): add EmailAndResume specs

--- libs/shared/ui/src/lib/molecules/email-and-resume-box/email-and-resume-box.spec.tsx
+++ libs/shared/ui/src/lib/molecules/email-and-resume-box/email-and-resume-box.spec.tsx
@@ -1,10 +1,31 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import EmailAndResumeBox from './email-and-resume-box';
 
+const email = 'someone@example.com';
+const resumeFileName = 'Resume-Someone.pdf';
+
 describe('EmailAndResumeBox', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<EmailAndResumeBox />);
+    const { baseElement } = render(
+      <EmailAndResumeBox email={email} resumeFileName={resumeFileName} />
+    );
+
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render with a link to the email passed as a prop', () => {
+    render(<EmailAndResumeBox email={email} resumeFileName={resumeFileName} />);
+
+    const anchorElement = screen.getByText(email).closest('a');
+    expect(anchorElement).toHaveAttribute('href', `mailto:${email}`);
+  });
+
+  it('should render with a link to the resume file passed as a prop', () => {
+    render(<EmailAndResumeBox email={email} resumeFileName={resumeFileName} />);
+
+    const anchorElement = screen.getByText(resumeFileName).closest('a');
+    expect(anchorElement).toHaveAttribute('href', `/${resumeFileName}`);
   });
 });
