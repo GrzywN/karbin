67fdec2880207804be02d9374885b064f09dab13
Karol Binkowski <starystachu9@gmail.com>
2023-02-04 17:17:28 +0100

    feat(site): add Work site

---
+++ apps/site/pages/work/index.tsx
@@ -0,0 +1,56 @@
+import WorkSection from '../../components/work-section/work-section';
+
+import imageBorKurPreview from '../../public/images/work/BorKurPreview.png';
+import imagePasjaPreview from '../../public/images/work/PasjaPreview.png';
+import imageYoutubeDownloaderPreview from '../../public/images/work/YoutubeDownloaderPreview.png';
+
+export function WorkPage() {
+  return (
+    <WorkSection
+      sectionTitle="My work"
+      heading="My work"
+      projects={[
+        {
+          title: 'BOR-KUR.pl',
+          description: `
+                Dedicated website made for a chicken farm to increase its
+                network of potential customers. It contains valuable information
+                to encourage potential contacts and mutual cooperation. It was
+                enhanced with additional features like a dark mode,
+                multilingualism and a password-protected link shortener on
+                request.`,
+          imageSrc: imageBorKurPreview,
+          imageAlt: 'A BOR-KUR website preview.',
+          tags: ['Next.js', 'TypeScript', 'Contentful', 'i18n'],
+          linkPreview: 'https://bor-kur.pl/',
+          linkSource: 'https://github.com/GrzywN/bor-kur',
+        },
+        {
+          title: 'Studio Fryzur Pasja',
+          description: `
+                A dedicated website that serves as the local company's business card and aims
+                to make the company stand out among its competitors.
+                My services additionally included placement of the hair salon on Google maps
+                and promotion through Google Ads.`,
+          imageSrc: imagePasjaPreview,
+          imageAlt: "Studio Fryzur Pasja's website preview.",
+          tags: ['Next.js', 'Tailwind CSS', 'Google Maps API'],
+          linkPreview: 'https://studiofryzurpasja.pl/',
+          linkSource: 'https://github.com/GrzywN/pasja',
+        },
+        {
+          title: 'Youtube Downloader',
+          description: `Simple youtube downloader which makes downloading youtube videos, high-quality music from a playlist or by search results really easy. You don't need to use your browser to search for youtube links anymore or use any of the online services.`,
+          imageSrc: imageYoutubeDownloaderPreview,
+          imageAlt: 'A preview of Youtube Downloader app.',
+          tags: ['Electron', 'JavaScript', 'Bulma'],
+          linkPreview:
+            'https://raw.githubusercontent.com/GrzywN/Youtube-Downloader/main/screenshot.gif',
+          linkSource: 'https://github.com/GrzywN/Youtube-Downloader',
+        },
+      ]}
+    />
+  );
+}
+
+export default WorkPage;
