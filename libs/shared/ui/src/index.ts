0715efe7406448af045958b1d4007efa4c7c367f
Karol Binkowski <starystachu9@gmail.com>
2023-02-19 11:46:23 +0100

    feat(shared-ui): add use-copy-to-clipboard hook

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/hooks/use-copy-to-clipboard/use-copy-to-clipboard';
 export * from './lib/molecules/article-navigation-button/article-navigation-button';
 export * from './lib/molecules/stack/stack';
 export * from './lib/molecules/category-list-box/category-list-box';
