3b01555cef164cb65a15f535b495068b37d1116a
Karol Binkowski <starystachu9@gmail.com>
2023-02-23 18:50:52 +0100

    feat(*): add "new-article" generator with schema

---
+++ tools/generators/new-article/index.ts
@@ -0,0 +1,33 @@
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  generateFiles,
+  joinPathFragments,
+  names,
+} from '@nrwl/devkit';
+
+interface NewArticleSchemaOptions {
+  title: string;
+  author: string;
+  excerpt?: string;
+  tags: string[];
+}
+
+export default async function (tree: Tree, schema: NewArticleSchemaOptions) {
+  generateFiles(
+    tree,
+    joinPathFragments(__dirname, './files'),
+    './content/articles',
+    {
+      title: schema.title,
+      author: schema.author,
+      excerpt: schema.excerpt || '',
+      tags: schema.tags,
+      normalizedTitle: names(schema.title).fileName,
+      creationDate: new Date().toISOString(),
+    }
+  );
+
+  await formatFiles(tree);
+}
