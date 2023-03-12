8852b99cd81fbeb93e260915cfd1046b3fe0c322
Karol Binkowski <starystachu9@gmail.com>
2023-03-12 19:55:02 +0100

    feat(site): add QuickIntroductionSection translations

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -1,28 +1,31 @@
-import QuickIntroductionSection from '../components/quick-introduction-section/quick-introduction-section';
-import MyWorkSection from '../components/my-work-section/my-work-section';
+import { getStringifiedJsonContent } from '@karbin/json-content';
+import { join } from 'path';
 import BlogPostsSection from '../components/blog-posts-section/blog-posts-section';
 import ContactSection from '../components/contact-section/contact-section';
+import MyWorkSection from '../components/my-work-section/my-work-section';
+import QuickIntroductionSection, {
+  QuickIntroductionSectionProps,
+} from '../components/quick-introduction-section/quick-introduction-section';
 
 import imageBorKur from '../public/images/work/BorKur.png';
-import imagePasja from '../public/images/work/Pasja.png';
-import imageYoutubeDownloader from '../public/images/work/YoutubeDownloader.png';
 import imageBorKurPreview from '../public/images/work/BorKurPreview.png';
+import imagePasja from '../public/images/work/Pasja.png';
 import imagePasjaPreview from '../public/images/work/PasjaPreview.png';
+import imageYoutubeDownloader from '../public/images/work/YoutubeDownloader.png';
 import imageYoutubeDownloaderPreview from '../public/images/work/YoutubeDownloaderPreview.png';
 
-export function Index() {
+const CONTENT_PATH = join(process.cwd(), 'content/site');
+
+export interface IndexProps {
+  quickIntroductionSectionContent: QuickIntroductionSectionProps;
+}
+
+export function Index(props: IndexProps) {
+  const { quickIntroductionSectionContent } = props;
+
   return (
     <>
-      <QuickIntroductionSection
-        sectionTitle="About"
-        text={
-          <>
-            Hello 👋 I&apos;m a Junior Front-end developer strongly <br />
-            focused on
-            <strong> React/React Native</strong> apps development.
-          </>
-        }
-      />
+      <QuickIntroductionSection {...quickIntroductionSectionContent} />
       <MyWorkSection
         sectionTitle="My work"
         heading="My work 👇"
@@ -114,6 +117,19 @@ export function Index() {
       />
     </>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  const quickIntroductionSectionContent = getStringifiedJsonContent(
+    locale,
+    join(CONTENT_PATH, 'quick-introduction-section')
+  ) as QuickIntroductionSectionProps;
+
+  return {
+    props: {
+      quickIntroductionSectionContent,
+    },
+  };
 }
 
 export default Index;
