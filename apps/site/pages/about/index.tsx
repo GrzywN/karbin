a704c839a1f37e11a85d70d8f85c2ed796d74fb4
Karol Binkowski <starystachu9@gmail.com>
2023-03-19 16:40:41 +0100

    feat(site): add AboutSection and MyToolsSection translations

--- apps/site/pages/about/index.tsx
+++ apps/site/pages/about/index.tsx
@@ -1,108 +1,47 @@
-import AboutSection from '../../components/about-section/about-section';
-import MyToolsSection from '../../components/my-tools-section/my-tools-section';
+import { getStringifiedJsonContent } from '@karbin/json-content';
+import { join } from 'path';
+import AboutSection, {
+  AboutSectionProps,
+} from '../../components/about-section/about-section';
+import MyToolsSection, {
+  MyToolsSectionProps,
+} from '../../components/my-tools-section/my-tools-section';
 
 import imageKarolBinkowski from '../../public/images/avatars/KarolBinkowski.png';
 
-export function AboutPage() {
+const CONTENT_PATH = join(process.cwd(), 'content/site');
+
+export interface AboutPageProps {
+  aboutSectionContent: AboutSectionProps;
+  myToolsSectionContent: MyToolsSectionProps;
+}
+
+export function AboutPage(props: AboutPageProps) {
+  const { aboutSectionContent, myToolsSectionContent } = props;
+
   return (
     <>
-      <AboutSection
-        sectionTitle="About me"
-        heading="About me"
-        paragraphs={[
-          <>
-            Hello 👋I&apos;m a <strong>Junior Front-end developer</strong>{' '}
-            strongly focused on <strong>React/React Native</strong> apps
-            development, but I can adapt to whatever tools are required.
-          </>,
-          <>
-            I currently have several commercial projects under my belt and have
-            already contributed to open-source codebases like
-            <strong> Nx</strong>. I live in <strong>Poland</strong>, but I would
-            be happy to work remotely.
-          </>,
-          <>
-            When I don&apos;t code, you&apos;ll find me outdoors. I love being
-            out in nature whether that&apos;s going for a walk, biking, reading
-            books or just hanging out with my friends.
-          </>,
-        ]}
-        imageSrc={imageKarolBinkowski}
-        imageAlt="Karol Binkowski"
-      />
-      <MyToolsSection
-        sectionTitle="My tools"
-        heading="My tools"
-        categories={[
-          {
-            heading: 'Frontend',
-            list: [
-              <>
-                <abbr title="HyperText Markup Language">HTML</abbr> and{' '}
-                <abbr title="Cascading Style Sheets">CSS</abbr>
-              </>,
-              'JavaScript',
-              'Next.js',
-              'React',
-              'Tailwind CSS',
-              'TypeScript',
-            ],
-          },
-          {
-            heading: 'Design',
-            list: [
-              'Design Systems',
-              'Figma',
-              'Responsive Web Design',
-              'Storybook',
-              <>
-                <abbr title="Web Content Accessibility Guidelines">WCAG</abbr>
-              </>,
-            ],
-          },
-          {
-            heading: 'Approaches',
-            list: [
-              'Atomic Design',
-              <>
-                <abbr title="Block Element Modifier">BEM</abbr>
-              </>,
-              'Design Patterns',
-              'Test-driven development',
-              <>
-                <abbr title="Web Content Accessibility Guidelines">WCAG</abbr>
-              </>,
-            ],
-          },
-          {
-            heading: 'Testing',
-            list: [
-              'Cypress',
-              'Jest',
-              'React Testing Library',
-              'Test-driven development',
-              'Vitest',
-            ],
-          },
-          {
-            heading: 'Integrations',
-            list: [
-              'Google Analytics',
-              'Google Maps',
-              <>
-                Headless <abbr title="Content Management System">CMS</abbr>
-              </>,
-              'Open Street Maps',
-            ],
-          },
-          {
-            heading: 'Workflow',
-            list: ['Figma', 'GIMP', 'Git', 'Linux', 'Nx workspaces', 'Vim'],
-          },
-        ]}
-      />
+      <AboutSection {...aboutSectionContent} imageSrc={imageKarolBinkowski} />
+      <MyToolsSection {...myToolsSectionContent} />
     </>
   );
 }
+export async function getStaticProps({ locale }) {
+  const aboutSectionContent = getStringifiedJsonContent(
+    locale,
+    join(CONTENT_PATH, 'about-section')
+  ) as AboutSectionProps;
 
+  const myToolsSectionContent = getStringifiedJsonContent(
+    locale,
+    join(CONTENT_PATH, 'my-tools-section')
+  ) as MyToolsSectionProps;
+
+  return {
+    props: {
+      aboutSectionContent,
+      myToolsSectionContent,
+    },
+  };
+}
 export default AboutPage;
